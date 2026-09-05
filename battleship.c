fd33b46f5bef81c12f97bf3ab12408ea3816dc2a
IT26101336 <IT26101336@my.sliit.lk>
2026-09-06 01:18:57 +0530

    activity day 2

--- battleship.c
+++ battleship.c
@@ -1,3 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+
 typedef struct
 {
     char type;
@@ -21,4 +26,269 @@ typedef struct
     double impactPower;
     int alive;
 } Escort;
+
+int main()
+{
+    double battlefieldSize;
+
+    int numberOfEscorts;
+
+    Battleship battleship;
+
+    Escort *escorts;
+
+    srand(time(NULL));
+
+    printf("========================================\n");
+    printf("       ADVANCED NAVAL BATTLE\n");
+    printf("========================================\n\n");
+
+    printf("Enter battlefield dimension: ");
+    scanf("%lf", &battlefieldSize);
+
+    printf("Enter number of Escort ships: ");
+    scanf("%d", &numberOfEscorts);
+
+    escorts = malloc(numberOfEscorts * sizeof(Escort));
+
+    if (escorts == NULL)
+    {
+    printf("Memory allocation failed.\n");
+    return 1;
+    }
+
+    printf("\nChoose Battleship type:\n");
+    printf("U - USS Iowa\n");
+    printf("M - MS King George V\n");
+    printf("R - Richelieu\n");
+    printf("S - Sovetsky Soyuz-class\n");
+
+    printf("\nEnter choice: ");
+    scanf(" %c", &battleship.type);
+
+    battleship.x = ((double)rand() / RAND_MAX) * battlefieldSize;
+    battleship.y = ((double)rand() / RAND_MAX) * battlefieldSize;
+
+    battleship.alive = 1;
+
+    printf("\nBattleship created!\n");
+    printf("Battleship type: %c\n", battleship.type);
+    printf("Battleship position: (%.2f, %.2f)\n",
+           battleship.x, battleship.y);
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
